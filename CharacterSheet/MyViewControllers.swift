6931efd35e52cf85a7ac16157297fcdfefc9e947
Taylor Nelms <taylornelms@tnelms-macbook-2.local>
2016-01-26 21:17:32 -0500

    Made better ability score view. Starting to get some functionality with the ability scores.

--- CharacterSheet/MyViewControllers.swift
+++ CharacterSheet/MyViewControllers.swift
@@ -12,6 +12,60 @@ class SummaryViewController: CSViewController{
     
 }//SummaryViewController
 
-class AbilityScoreViewController: CSViewController{
+class AbilityScoreViewController: CSViewController, UITextFieldDelegate{
+    
+    //MARK: Properties
+    var scores: AScores = AScores.init();
+    
+    //MARK: Outlets
+    
+    @IBOutlet weak var strTextField: UITextField!
+    @IBOutlet weak var dexTextField: UITextField!
+    @IBOutlet weak var conTextField: UITextField!
+    @IBOutlet weak var intTextField: UITextField!
+    @IBOutlet weak var wisTextField: UITextField!
+    @IBOutlet weak var chaTextField: UITextField!
+    
+    @IBOutlet weak var strModLabel: UILabel!
+    @IBOutlet weak var dexModLabel: UILabel!
+    @IBOutlet weak var conModLabel: UILabel!
+    @IBOutlet weak var intModLabel: UILabel!
+    @IBOutlet weak var wisModLabel: UILabel!
+    @IBOutlet weak var chaModLabel: UILabel!
+    
+    //MARK: Actions
+    
+    @IBAction func setStrValue(sender: UITextField) {
+        sender.resignFirstResponder();
+        let newValue: Int = NSNumberFormatter().numberFromString(sender.text!)!.integerValue;
+        scores.setStr(newValue);
+        sender.text = "\(newValue)";//force an int conversion to cut down on trash input
+        let modval: Int = scores.getStrMod();
+        if (modval >= 0){
+            strModLabel.text = "+\(modval)";
+        }//if
+        else{
+            strModLabel.text = "\(modval)";
+        }//else
+    }
+    @IBAction func setDexValue(sender: UITextField) {
+    }
+    @IBAction func setConValue(sender: UITextField) {
+    }
+    @IBAction func setIntValue(sender: UITextField) {
+    }
+    @IBAction func setWisValue(sender: UITextField) {
+    }
+    @IBAction func setChaValue(sender: UITextField) {
+    }
+
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+
     
 }//AbilityScoreViewController
